f7d67c577af9f2fba147f4709df61c357f24da4e
Rinkit Adhana <therinkit@gmail.com>
2024-08-12 03:19:07 +0530

    add

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -13,11 +13,11 @@ const Tree = () => {
       <div className="flex justify-center md:flex-row flex-col  ">
         <div className=" md:w-2/5 ">
           <div className="flex flex-row justify-between mt-6 px-2">
-            <div className=" font-bold text-green-500 text-2xl  font-bricolage ">
+            <div className=" font-bold text-green-500 text-3xl  font-bricolage ">
               <span className=" text-gray-600">Link</span>It
             </div>
             <a href="https://github.com/rinkitadhana/LinkFree" target=" _blank">
-              <div className="flex flex-row gap-1 px-2 py-1 bg-black/40 w-fit items-center rounded-lg font-medium text-white text-sm hover:bg-black/30 ">
+              <div className="flex flex-row gap-1 p-2 bg-black/40 w-fit items-center rounded-lg font-medium text-white text-sm hover:bg-black/30 ">
                 <i className="fa-solid fa-star text-yellow-400"></i>
                 <div>on Github</div>
               </div>
